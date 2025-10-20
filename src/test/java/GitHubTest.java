31d49192b1b547855a38dc0017dc017301e75fe9
pepd62 <zheltovdmitrij@gmail.com>
2025-10-20 21:50:04 +0300

    Добавлены параметры

---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class GitHubTest {
+    @Test
+    void test01(){
+        open("https://github.com/pepd62/at-java-study.git");
+        $("body").shouldHave(text("gradle/wrapper"));
+    }
+    @Test
+    void test02(){
+        open("https://github.com/pepd62/at-java-study.git");
+        $("body").shouldHave(text("src/test/java"));
+    }
+}
